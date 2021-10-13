c2897fcc4d9257a3ff4c4b3ae68da18ae5dd9911
iDenis124 <denisg.co2.1@gmail.com>
2021-10-13 11:31:22 +0300

    added kromer (no database yet)

---
+++ messageCommands/kromer.js
@@ -0,0 +1,24 @@
+module.exports = {
+  method: function (message, Bot, args) {
+    let reply;
+
+    if (args.length < 2) {
+      return message.channel.send('Usage is: `iM!kromer [add/remove] [user ping] [amount of kromer]`');
+    }
+
+    const user = args[1].split('@!')[1].split('>')[0]; // gets user ID from ping
+    // we save the ID instead of user for the database
+
+    if (args[0].toLowerCase() === 'add')
+      reply = `Added ${args[2]} kromer to ${Bot.users.cache.get(user)}`;
+    else if (args[0].toLowerCase() === 'remove')
+      reply = `Removed ${args[2]} kromer from ${Bot.users.cache.get(user)}`;
+    else {
+      return message.channel.send('invalid command');
+      // here we return directly to stop
+      // execusion if the command is bad
+    }
+
+    return message.channel.send(reply);
+  }
+};
