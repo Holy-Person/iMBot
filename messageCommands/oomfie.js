34bbde1d0c8761682b44fb8c29a708abf02d248b
iDenis124 <denisg.co2.1@gmail.com>
2021-10-13 23:17:54 +0300

    I'm living in your walls oomfie

--- messageCommands/oomfie.js
+++ messageCommands/oomfie.js
@@ -1,13 +1,24 @@
+var fs = require("fs");
+
 module.exports = {
 	method: function (message, _Bot) {
 		// const variants = ['oomfie', 'coomfie', 'comfie'];
 		// better than trying a bunch of variants like above
 		if (message.content.includes('mfie')) {
-			// 1 in 20 chance of replying with "coomfie" instead
-			const reply = (Math.floor(Math.random() * 20) > 0) ? 'oomfie' : 'coomfie';
-			return message.channel.send(`so true ${reply}`);
+			let reply;
+			if (Math.floor(Math.random() * 50) == 0) {
+				fs.readFile('oomfie.txt', function (err, data) {
+					if (err) throw err;
+					let lines = data.toString().split('\n');
+					return message.channel.send(lines[Math.floor(Math.random() * lines.length)].toString());
+				});
+			} else {
+				if (Math.floor(Math.random() * 20) == 0) {
+					// 1 in 20 chance of replying with "coomfie" instead
+					message.channel.send('so true coomfie');
+				}
+				message.channel.send('so true oomfie');
+			}
 		}
-
-		return;
 	}
 };
