98606ed0a4dde3426a15b1034fde64aeb8615016
Holy-Person <44945796+Holy-Person@users.noreply.github.com>
2021-10-13 18:18:45 +0200

    Fixed a non-used require.

--- messageCommands/reboot.js
+++ messageCommands/reboot.js
@@ -1,12 +1,11 @@
 const { spawn } = require('child_process');
-const idList = require('../idList.js');
 require('../dotenv').config();
 
 module.exports = {
   method: function(message, Bot, args) {
 
     if(!process.env.ADMIN_USERS.split(",").includes(message.author.id)) {return message.channel.send(`Sorry ${message.author.username}, you're not allowed to use that command.`); }
-    
+
     const reboot = spawn('sudo git pull && pm2 start deploy-commands.js --no-autorestart && pm2 restart iMBot.js', {
       shell: true
     });
