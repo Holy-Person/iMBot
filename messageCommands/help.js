ee058ea7a5f9289079e1ee7ec0be608df690e5f1
Holy-Person <44945796+Holy-Person@users.noreply.github.com>
2021-10-14 22:15:14 +0200

    Remove logging
    Works now!

--- messageCommands/help.js
+++ messageCommands/help.js
@@ -10,7 +10,7 @@ const messageCommands = new Array();
 
 /*messageCommands.sort(function(a, b){
     return a.name - b.name;
-});*/
+});*/ //Use sorting for paging
 
 module.exports = {
   description: `Shows general help for a given command.`,
@@ -18,15 +18,11 @@ module.exports = {
   method: function (message, _Bot, args) {
     for (const file of messageCommandFiles) {
       const command = require(`../messageCommands/${file}`);
-      console.log(command);
-      console.log(command.name, command.description, command.usage, command.method);
       const {
         name: name = file.split('.js')[0],
         description: description = "No description provided.",
         usage: usage = "No usage provided."
       } = command;
-
-      console.log(name, description, usage);
 
       let messageCommand = {
         "name": name,
@@ -34,23 +30,21 @@ module.exports = {
         "usage": usage
       };
 
-      console.log(messageCommand.name, messageCommand.description, messageCommand.usage);
-
     	messageCommands.push(messageCommand);
-    }
-    
+    } //There's probably 50000 ways to do this more optimized but I'm at my limit for today.
+
     const commandObject = messageCommands.find(x => x.name === args[0]);
-    console.log(args[0], commandObject, commandObject.description);
     if(typeof commandObject != 'undefined') {
       const CommandEmbed = new MessageEmbed()
+        .setColor('#484C92');
         .setTitle(args[0])
-        .addField("Description" , commandObject.description);
-      CommandEmbed.setColor('#484C92');
+        .addField("Description", commandObject.description);
+        .addField("Usage", commandObject.usage);
       return message.channel.send({ embeds: [CommandEmbed] });
     }
     const HelpEmbed = new MessageEmbed()
-      .setColor('#484C92')
-      .setTitle(`Command List`)
+      .setTitle(`Command List`);
+    HelpEmbed.setColor('#484C92');//This also works btw, use for filling pages.
     return message.channel.send({ embeds: [HelpEmbed] });
   }
 };
