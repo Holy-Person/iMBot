f447690e9c561e0c50c3794d446442ad9a5bc6ed
Holy-Person <44945796+Holy-Person@users.noreply.github.com>
2021-10-14 16:22:25 +0200

    Upgraded to newer Embed

--- messageCommands/help.js
+++ messageCommands/help.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-const { Discord } = require("discord.js");
+const { MessageEmbed } = require('discord.js');
 
 module.exports = {
   description: `Lists all help commands.`,
@@ -15,15 +15,21 @@ module.exports = {
       const name = file.slice(0, -3);
       messageCommands[name] = command;
     }
+
     if(messageCommands[args]) {
-      return message.channel.send(messageCommands[args].description);
+      const CommandEmbed = new MessageEmbed()
+        .setColor('#484C92')
+        .setTitle(args)
+        .addField("Description" , messageCommands[args].description)
+        .setTimestamp();
+      return message.channel.send({ embeds: [CommandEmbed] });
     }
-    const HelpEmbed = new Discord.RichEmbed()
-      HelpEmbed.setColor('#484C92');
-      HelpEmbed.setTitle(`Title1`);
-      HelpEmbed.addField("Field2_1" , `Field2_2`);
-      HelpEmbed.setTimestamp();
-    return message.channel.send(HelpEmbed);
+    const HelpEmbed = new MessageEmbed()
+      .setColor('#484C92')
+      .setTitle(`Title1`)
+      .addField("Field2_1" , `Field2_2`)
+      .setTimestamp();
+    return message.channel.send({ embeds: [HelpEmbed] });
   }
 };
 
