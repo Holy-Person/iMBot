d433a2b43c0e70095305c9f069d6f361fe7c4b23
Holy-Person <44945796+Holy-Person@users.noreply.github.com>
2021-10-24 14:56:46 +0200

    Use message.member instead of author

--- messageCommands/role.js
+++ messageCommands/role.js
@@ -6,7 +6,11 @@ module.exports = {
   method: function (message, Bot, args) {
     let roleNeos = message.guild.roles.cache.find(role => role.id === '855574981048008705');
 
-    message.author.roles.remove(roleNeos.id);
-    return message.channel.send(`Removed the ${roleNeos.name} role.`);
+    if (message.member.roles.cache.has(roleNeos.id)) {
+      message.member.roles.remove(roleNeos.id); //message.member gets a member instead of a user, members are unique to guilds while users aren't. ex: message.author
+      return message.channel.send(`Removed the ${roleNeos.name} role.`);
+    }
+
+    return message.channel.send(`Could not remove the ${roleNeos.name} role, you do not have it.`);
   }
 };
