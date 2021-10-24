07f0be6558e7b0fc1121ab49c1abc0897f551c18
Holy-Person <44945796+Holy-Person@users.noreply.github.com>
2021-10-24 14:26:24 +0200

    Now uses message.author

--- messageCommands/role.js
+++ messageCommands/role.js
@@ -1,5 +1,4 @@
 require("dotenv").config();
-const commonFunctions = require('../commonFunctions.js');
 
 module.exports = {
   description: `Lets users assign and remove roles on themselves.`,
@@ -7,13 +6,7 @@ module.exports = {
   method: function (message, Bot, args) {
     let roleNeos = message.guild.roles.cache.find(role => role.id === '855574981048008705');
 
-    let user = commonFunctions.getUserFromMention(args[1], Bot);
-
-    if (!user) {
-      return message.channel.send(`Couldn't find that role, try checking the name again.`);
-    }
-
-    user.roles.remove(roleNeos.id);
+    message.author.roles.remove(roleNeos.id);
     return message.channel.send(`Removed the ${roleNeos.name} role.`);
   }
 };
