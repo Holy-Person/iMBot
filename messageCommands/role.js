3e11dacbf00d565fb75ca17903db3aee9e467338
Holy-Person <44945796+Holy-Person@users.noreply.github.com>
2021-10-28 03:34:03 +0200

    selfRoles in config and role toggle
    Still missing blocklists, but this should work for now.

--- messageCommands/role.js
+++ messageCommands/role.js
@@ -3,15 +3,20 @@ const Config = require('../config.json');
 module.exports = {
   description: `Lets users assign and remove roles on themselves.`,
   usage: `Usage \`${Config.prefix}role\`.`,
-  method: function (message, Bot, args) {
+  method: function (message, _Bot, args) {
 
-    let roleNeos = message.guild.roles.cache.find(role => role.id === '855574981048008705');
-
-    if (message.member.roles.cache.has(roleNeos.id)) {
-      message.member.roles.remove(roleNeos.id); //message.member gets a member instead of a user, members are unique to guilds while users aren't. ex: message.author
-      return message.channel.send(`Removed the "${roleNeos.name}" role.`);
+    const foundRoleObject = Config.selfRoles.find(x => x.name.toLowerCase() === args[0]);
+    if (typeof foundRole == 'undefined') {
+      return message.channel.send(`Sorry, I couldn't find that role.`);
     }
+    const foundRole = message.guild.roles.cache.find(role => role.id === foundRoleObject.id);
 
-    return message.channel.send(`Could not remove the "${roleNeos.name}" role, you do not have it.`);
+    if (message.member.roles.cache.has(foundRole.id)) {
+      message.member.roles.remove(foundRole.id); //message.member gets a member instead of a user, members are unique to guilds while users aren't. ex: message.author
+      return message.channel.send(`Removed the "${foundRole.name}" role.`);
+    } else {
+      message.member.roles.add(foundRole.id);
+      return message.channel.send(`Added the "${foundRole.name}" role.`);
+    }
   }
 };
