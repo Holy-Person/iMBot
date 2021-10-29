5924528760b81a2a3ba04cfa880bee682d096589
Holy-Person <44945796+Holy-Person@users.noreply.github.com>
2021-10-29 23:38:23 +0200

    Update role.js

--- messageCommands/role.js
+++ messageCommands/role.js
@@ -5,11 +5,19 @@ module.exports = {
   usage: `Usage \`${Config.prefix}role\`.`,
   method: function (message, _Bot, args) {
 
+    if (args[0]) {
+      return message.channel.send(`Default help text due to no args`);
+    }
+
     const foundRoleObject = Config.selfRoles.find(x => x.name.toLowerCase() === args[0]);
+
     if (typeof foundRoleObject == 'undefined') {
       return message.channel.send(`Sorry, I couldn't find that role.`);
     }
-    const foundRole = message.guild.roles.cache.find(role => role.id === foundRoleObject.id);
+
+    let foundRole = message.guild.roles.cache.find(r => r.id === foundRoleObject.id);
+
+
 
     if (message.member.roles.cache.has(foundRole.id)) {
       message.member.roles.remove(foundRole.id); //message.member gets a member instead of a user, members are unique to guilds while users aren't. ex: message.author
