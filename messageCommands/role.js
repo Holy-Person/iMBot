f25f3e29b7689d1868e5f0d55d1c338cc352b26c
Holy-Person <44945796+Holy-Person@users.noreply.github.com>
2021-11-05 23:48:36 +0100

    Test without fieldtitle

--- messageCommands/role.js
+++ messageCommands/role.js
@@ -11,7 +11,7 @@ module.exports = {
         .setColor('#484C92')
         .setTitle(`Available Selfroles`);
       Config.selfRoles.forEach((role, i) => {
-        ListEmbed.addField(``, `<@&${role.id}> - ${role.name}`);
+        ListEmbed.addField(`<@&${role.id}> - ${role.name}`);
       });
       return message.channel.send(`Please provide a role name you want to add/remove.\nUsage: ${Config.prefix}role [roleName]\n`+ListEmbed);
       //This is a very temporary solution.
