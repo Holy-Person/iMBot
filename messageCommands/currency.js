6e61310e176faaf08c1fd3c053081482c00b01b4
Holy-Person <44945796+Holy-Person@users.noreply.github.com>
2021-11-15 19:58:22 +0100

    Test of getting saved currency value.

--- messageCommands/currency.js
+++ messageCommands/currency.js
@@ -5,19 +5,36 @@ module.exports = {
   description: `Description not set.`,
   usage: `Usage not set.`,
   method: async function (message, Bot, args, Database) {
-		try {
-			const test = await Database.create({
-				user: message.author.id,
-        balance: 0.1,
-			});
+    switch (args[0]) {
+      case 'add':
+        try {
+          const test = await Database.create({
+            user: message.author.id,
+            balance: 0.1,
+          });
 
-			return message.channel.send(`You now have 0.1 currency.`);
-		} catch (error) {
-			if (error.name === 'SequelizeUniqueConstraintError') {
-				return message.channel.send('Already got currency.');
-			}
+          return message.channel.send(`You now have 0.1 currency.`);
+        } catch (error) {
+          if (error.name === 'SequelizeUniqueConstraintError') {
+            return message.channel.send('Already got currency.');
+          }
 
-			return message.channel.send('Something else went wrong.');
-		}
+          return message.channel.send('Something else went wrong.');
+        }
+        break;
+      case 'view':
+        const test = await Database.findOne({ where: { user: message.author.id } });
+
+        if (test) {
+        	return message.channel.send(`You currenly have `+test.get('balance') + `currency.`);
+        }
+
+        return message.channel.send(`Could not find your money. Are you poor?`);
+
+        break;
+      default:
+        return message.channel.send('Please define args like add/view.');
+    }
+
   }
 };
