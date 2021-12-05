b7378246bd48bbd850153d5b41b0b87a00967d3f
Holy-Person <44945796+Holy-Person@users.noreply.github.com>
2021-12-05 20:42:19 +0100

    Merge from new hardware, broke a few things, should work soon I think

--- currencyInteractions.js
+++ currencyInteractions.js
@@ -9,7 +9,11 @@ module.exports = {
         balance: Amount,
       });
 
+<<<<<<< Updated upstream
       return "create"; //Created database entry
+=======
+      return 0; //Created database entry
+>>>>>>> Stashed changes
     } catch (error) {
       if (error.name === 'SequelizeUniqueConstraintError') {
         const recievingUser = await Database.findOne({ where: { user: TargetID } });
@@ -19,7 +23,11 @@ module.exports = {
         const affectedEntries = await Database.update({ balance: newBalance }, { where: { user: TargetID } });
 
         if (affectedEntries > 0) {
+<<<<<<< Updated upstream
           return "update"; //Updated entry.
+=======
+          return 1; //Updated entry.
+>>>>>>> Stashed changes
         }
 
         return 2; //Other error, didn't update.
@@ -28,10 +36,17 @@ module.exports = {
       return 3; //Other error, didn't do anything.
     }
   },
+<<<<<<< Updated upstream
   remove: async function (Database, Amount, Operator, Target) {
     //empty
   },
   transfer: async function (Database, Amount, Operator, Target) {
+=======
+  remove: async function (Database, Amount, OperatorID, TargetID) {
+    //empty
+  },
+  transfer: async function (Database, Amount, OperatorID, TargetID) {
+>>>>>>> Stashed changes
     //empty
   }
 }
