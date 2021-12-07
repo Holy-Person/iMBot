707314b9882dd94d24d7bdf00c335180d50236c2
Holy-Person <44945796+Holy-Person@users.noreply.github.com>
2021-12-07 23:55:02 +0100

    Addition of a rounding method for currency etc.

--- commonFunctions.js
+++ commonFunctions.js
@@ -7,5 +7,9 @@ module.exports = {
     if (mention.startsWith('<@') && mention.endsWith('>')) {
       return Bot.users.cache.get(validate.exec(mention)[0]); //Returns undefined if user cannot be found.
     }
+  },
+  betterRound: function(number, decimalPlaces) {
+    const powTen = Math.pow(10, decimalPlaces);
+    return Math.round(number * powTen) / powTen;
   }
 }
